27b606e6f6c8505bf22887987fe5b76a145f7b66
Lucas Vital <lucasvitaldemelo@gmail.com>
2025-06-20 01:43:35 -0300

    feat: add `swagger`

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import * as cookieParser from 'cookie-parser';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
@@ -14,6 +15,17 @@ async function bootstrap() {
 
   app.enableCors(corsOptions)
   app.use(cookieParser())
+
+  const config = new DocumentBuilder()
+    .setTitle('Manager API')
+    .setDescription('Manager API description')
+    .setVersion('1.0')
+    .addCookieAuth('access_token') // Suporte a cookie como auth
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs-swagger', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
